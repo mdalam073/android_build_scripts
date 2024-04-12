e645edb4529b6ba53e3957ff71f199e90455aab2
mdalam073 <151665023+mdalam073@users.noreply.github.com>
2024-04-12 06:16:21 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,23 +2,52 @@
 
 set -e
 
-# Remove existing local_manifests (if needed)
-rm -rf .repo/local_manifests
+# Initialize repo with specified manifest
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --depth=1
 
-# Initialize repo with specified manifest for PixelExperience
-repo init -u https://github.com/PixelExperience/manifest -b fourteen --depth=1 --platform="auto,emulator,kvm"
+# Run inside foss.crave.io devspace, in the project folder
+# Remove existing local_manifests
+crave run --no-patch -- "rm -rf .repo/local_manifests && \
+# Initialize repo with specified manifest
+repo init -u https://github.com/PixelExperience/manifest -b fourteen --depth=1 ;\
 
-# Clone your local_manifests repository
-git clone https://github.com/mdalam073/local_manifest --depth 1 -b PixelExperience-14 .repo/local_manifests
+# Clone local_manifests repository
+git clone https://github.com/mdalam073/local_manifest --depth 1 -b PixelExperience-14 .repo/local_manifests ;\
+
 
 # Sync the repositories
-/opt/crave/resync.sh
+/opt/crave/resync.sh && \
+
 
 # Set up build environment
-source build/envsetup.sh
+source build/envsetup.sh && \
 
 # Lunch configuration
-lunch aosp_tissot-userdebug
+lunch aosp_tissot-ap1a-userdebug ;\
 
-# Build the ROM
-mka bacon
+croot ;\
+mka bacon ; \
+# echo "Date and time:" ; \
+
+# Print out/build_date.txt
+# cat out/build_date.txt; \
+
+# Print SHA256
+# sha256sum out/target/product/*/*.zip"
+
+# Clean up
+# rm -rf tissot/*
+
+
+
+# Pull generated zip files
+# crave pull out/target/product/*/*.zip
+
+# Pull generated img files
+# crave pull out/target/product/*/*.img
+
+# Upload zips to Telegram
+# telegram-upload --to sdreleases tissot/*.zip
+
+#Upload to Github Releases
+#curl -sf https://raw.githubusercontent.com/Meghthedev/Releases/main/headless.sh | sh
