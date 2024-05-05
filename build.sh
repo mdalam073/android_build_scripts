0dd6939135692b9d58f49d2d28032439f0ef41e6
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-05-05 11:25:15 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,11 +6,12 @@ set -e
 repo init -u https://github.com/PixelOS-AOSP/manifest -b fourteen --git-lfs --depth=1
 
 # Run inside foss.crave.io devspace, in the project folder
+# Remove existing local_manifests
+crave run --no-patch -- "rm -rf .repo/local_manifests && \
 # Initialize repo with specified manifest
 repo init -u https://github.com/VoltageOS/manifest.git -b 14 --git-lfs --depth=1
 
-# Remove existing local_manifests
-crave run --no-patch -- "rm -rf .repo/local_manifests && \
+
 # Clone local_manifests repository
 git clone https://github.com/mdalam073/local_manifest --depth 1 -b voltageos-14 .repo/local_manifests && \
 
