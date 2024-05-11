ea513e2d87ee0e07a540577402b7dd0339c1a298
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-05-11 14:37:10 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,26 +3,23 @@
 set -e
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --depth=1
+repo init -u https://github.com/PixelOS-AOSP/manifest -b fourteen --git-lfs --depth=1
 
 # Run inside foss.crave.io devspace, in the project folder
 # Remove existing local_manifests
-crave run --clean --no-patch -- "rm -rf .repo/local_manifests && \
+crave run --no-patch -- "rm -rf .repo/local_manifests && \
 # Initialize repo with specified manifest
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --depth=1 ;\
 
-# Clone local_manifest repository
-git clone https://github.com/mdalam073/local_manifest.git -b Derp-14-tissot .repo/local_manifest ;\
-
+# Clone local_manifests repository
+git clone https://github.com/mdalam073/local_manifest --depth 1 -b Derp-14-tissot .repo/local_manifest ;\
 
 # Sync the repositories
-/opt/crave/resync.sh && \
+/opt/crave/resync.sh && \ 
 
-# Removals
-rm -rf platform/prebuilts/rust
 
 # Set up build environment
-. build/envsetup.sh && \
+source build/envsetup.sh && \
 
 # Lunch configuration
 lunch derp_tissot-ap1a-userdebug ;\
@@ -52,4 +49,9 @@ mka derp ; \
 # telegram-upload --to sdreleases tissot/*.zip
 
 #Upload to Github Releases
-#curl -sf https://raw.githubusercontent.com/Meghthedev/Releases/main/headless.sh | sh
+#curl -sf https://raw.githubusercontent.com/mdalam073/Releases/main/headless.sh | sh
+# Upload zips to Telegram
+# telegram-upload --to sdreleases tissot/*.zip
+
+#Upload to Github Releases
+#curl -sf https://raw.githubusercontent.com/mdalam073/Releases/main/headless.sh | sh
