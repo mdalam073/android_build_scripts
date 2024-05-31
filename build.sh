4534ced724aac24fa5ceb82aa8077e769d562d6e
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-05-31 07:12:16 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,10 +10,10 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs 
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs --depth=1 ;\
+repo init -u https://github.com/ProjectInfinity-X/manifest -b QPR2 --git-lfs --depth=1 ;\
 
 # Clone local_manifests repository
-git clone https://github.com/mdalam073/local_manifest --depth 1 -b superior-tissot .repo/local_manifests ;\
+git clone https://github.com/mdalam073/local_manifest --depth 1 -b infinity-x-tissot .repo/local_manifests ;\
 
 # Sync the repositories
 /opt/crave/resync.sh && \ 
@@ -23,7 +23,7 @@ git clone https://github.com/mdalam073/local_manifest --depth 1 -b superior-tiss
 . build/envsetup.sh && \
 
 # Lunch configuration
-breakfast tissot ;\
+lunch infinity_tissot-ap1a-userdebug ;\
 
 croot ;\
 m bacon ; \
