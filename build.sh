e2b643b01bbb78111fa358a91510f62391eb6f18
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-06-01 08:54:12 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,43 +6,34 @@ set -e
 repo init -u https://github.com/PixelOS-AOSP/manifest -b fourteen --git-lfs --depth=1
 
 # Run inside foss.crave.io devspace, in the project folder
+crave run --no-patch -- "
 # Remove existing local_manifests
-crave run --no-patch -- "rm -rf .repo/local_manifests && \
+rm -rf .repo/local_manifests && \
 # Initialize repo with specified manifest
-repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs --depth=1 ;\
-
+repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs --depth=1 && \
 # Clone local_manifests repository
-git clone https://github.com/mdalam073/local_manifest --depth 1 -b superior-tissot .repo/local_manifests ;\
-
+git clone https://github.com/mdalam073/local_manifest --depth 1 -b superior-tissot .repo/local_manifests && \
 # Sync the repositories
-/opt/crave/resync.sh && \ 
-repo sync --force-sync \
-
-
+/opt/crave/resync.sh && \
+repo sync --force-sync && \
 # Set up build environment
-. build/envsetup.sh && \
-
+source build/envsetup.sh && \
 # Clean the build environment
 make clean && \
-
+# Set the TARGET_RELEASE variable explicitly
+export TARGET_RELEASE=ap1a && \
 # Lunch configuration
-breakfast tissot ;\
-
+lunch superior_tissot-ap1a-userdebug && \
 # Change root to build environment
 croot && \
-
 # Build the target
 m bacon && \
-
 # Print SHA256 checksums of all .zip files in the output directory
 sha256sum out/target/product/*/*.zip && \
-
 # Pull generated .zip files
 crave pull out/target/product/*/*.zip && \
-
 # Pull generated .img files
 crave pull out/target/product/*/*.img && \
-
 # Upload to GitHub Releases using a script from the Releases repository
 curl -sf https://raw.githubusercontent.com/mdalam073/Releases/main/headless.sh | sh
 "
