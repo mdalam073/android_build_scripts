b1f82d7f084dfba47832dd66a3e5f8cc14458c48
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-06-04 23:49:05 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,35 +3,50 @@
 set -e
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/PixelOS-AOSP/manifest -b fourteen --git-lfs --depth=1
+repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen --git-lfs
 
 # Run inside foss.crave.io devspace, in the project folder
-crave run --no-patch -- "
 # Remove existing local_manifests
-rm -rf .repo/local_manifests && \
+crave run --no-patch -- "rm -rf .repo/local_manifests && \
 # Initialize repo with specified manifest
-repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs --depth=1 && \
+repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs --depth=1 && ;\
+
 # Clone local_manifests repository
 git clone https://github.com/mdalam073/local_manifest --depth 1 -b superior-tissot .repo/local_manifests && \
 # Sync the repositories
 /opt/crave/resync.sh && \
-repo sync --force-sync && \
+
+
 # Set up build environment
 source build/envsetup.sh && \
-# Clean the build environment
-make clean && \
+
 # Lunch configuration
-lunch superior_tissot-userdebug && \
-# Change root to build environment
-croot && \
-# Build the target
-m bacon && \
-# Print SHA256 checksums of all .zip files in the output directory
-sha256sum out/target/product/*/*.zip && \
-# Pull generated .zip files
-crave pull out/target/product/*/*.zip && \
-# Pull generated .img files
-crave pull out/target/product/*/*.img && \
-# Upload to GitHub Releases using a script from the Releases repository
-curl -sf https://raw.githubusercontent.com/mdalam073/Releases/main/headless.sh | sh
-"
+lunch superior_tissot-ap1a-userdebug ;\
+
+croot ;\
+repo forall -c 'git lfs install && git lfs pull && git lfs checkout' ;\
+m bacon ; \
+# echo "Date and time:" ; \
+
+# Print out/build_date.txt
+# cat out/build_date.txt; \
+
+# Print SHA256
+# sha256sum out/target/product/*/*.zip"
+
+# Clean up
+# rm -rf tissot/*
+
+
+
+# Pull generated zip files
+# crave pull out/target/product/*/*.zip
+
+# Pull generated img files
+# crave pull out/target/product/*/*.img
+
+# Upload zips to Telegram
+# telegram-upload --to sdreleases tissot/*.zip
+
+#Upload to Github Releases
+#curl -sf https://raw.githubusercontent.com/Meghthedev/Releases/main/headless.sh | sh
