6e1243ad6bf5f2a120b27cdcd0209d8e1d088f03
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-06-05 20:51:49 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,43 +2,42 @@
 
 set -e
 
-# Initialize repo with specified manifest
+# Initialize repo with the first manifest
 repo init -u https://github.com/PixelOS-AOSP/manifest -b fourteen --git-lfs --depth=1
 
 # Run inside foss.crave.io devspace, in the project folder
-# Remove existing local_manifests
-crave run --no-patch -- "rm -rf .repo/local_manifests && \
-# Initialize repo with specified manifest
-repo init -u https://github.com/SuperiorOS/manifest -b fourteen --git-lfs --depth=1 && \
-# Clone local_manifests repository
-git clone https://github.com/mdalam073/local_manifest --depth 1 -b superior-tissot .repo/local_manifests && \
-# Sync the repositories
-/opt/crave/resync.sh && \
-# Set up build environment
-source build/envsetup.sh && \
-# Lunch configuration
-lunch superior_tissot-ap1a-userdebug && \
-croot && \
-repo forall -c 'git lfs install && git lfs pull && git lfs checkout' && \
-m bacon && \
-# echo "Date and time:" && \
-# Print out/build_date.txt
-# cat out/build_date.txt && \
-# Print SHA256
-# sha256sum out/target/product/*/*.zip"
+crave run --no-patch -- "
+    # Remove existing local_manifests
+    rm -rf .repo/local_manifests && \
+    # Initialize repo with the SuperiorOS manifest
+    repo init -u https://github.com/SuperiorOS/manifest -b fourteen --git-lfs --depth=1 && \
+    # Clone local_manifests repository
+    git clone https://github.com/mdalam073/local_manifest --depth 1 -b superior-tissot .repo/local_manifests && \
+    # Sync the repositories
+    /opt/crave/resync.sh && \
+    # Set up build environment
+    source build/envsetup.sh && \
+    # Lunch configuration
+    lunch superior_tissot-ap1a-userdebug && \
+    # Change to the root directory
+    croot && \
+    # Install and pull Git LFS files
+    repo forall -c 'git lfs install && git lfs pull && git lfs checkout' && \
+    # Start the build process
+    m bacon
 "
 
-# Clean up
+# Clean up (optional)
 # rm -rf tissot/*
 
-# Pull generated zip files
+# Pull generated zip files (optional)
 # crave pull out/target/product/*/*.zip
 
-# Pull generated img files
+# Pull generated img files (optional)
 # crave pull out/target/product/*/*.img
 
-# Upload zips to Telegram
+# Upload zips to Telegram (optional)
 # telegram-upload --to sdreleases tissot/*.zip
 
-# Upload to Github Releases
+# Upload to Github Releases (optional)
 # curl -sf https://raw.githubusercontent.com/Meghthedev/Releases/main/headless.sh | sh
