d2e65fb35d2080179ea8efef5c0c98aaf78c1679
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-06-22 16:12:57 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,45 +7,37 @@ repo init -u https://github.com/PixelOS-AOSP/manifest -b fourteen --git-lfs --de
 
 # Run inside foss.crave.io devspace, in the project folder
 # Remove existing local_manifests
-crave run --no-patch -- "
-    # Clean and reset repo manually
-    echo 'Cleaning and resetting repo...' && \
-    rm -rf .repo/local_manifests && \
-    rm -rf .repo/project-objects/* && \
-    rm -rf .repo/projects/* && \
-    rm -rf .repo/repo/ && \
+crave run --no-patch -- "rm -rf .repo/local_manifests && \
+# Initialize repo with specified manifest
+repo init -u https://github.com/VoltageOS/manifest -b 14 --git-lfs --depth=1 && \
     
-    # Reinitialize repo with specified manifest
-    repo init -u https://github.com/VoltageOS/manifest.git -b 14 --git-lfs --depth=1 && \
+# Clone local_manifests repository
+git clone https://github.com/mdalam073/local_manifest --depth 1 -b voltageos-14 .repo/local_manifests && \
     
-    # Clone local_manifests repository
-    git clone https://github.com/mdalam073/local_manifest --depth 1 -b voltageos-14 .repo/local_manifests && \
-    
-    # Sync the repositories
-    repo sync -j$(nproc) --force-sync && \ 
-    
-    # Set up build environment
-    source build/envsetup.sh && \
-    
-    # Lunch configuration
-    lunch voltage_tissot-ap1a-userdebug && \
-    
-    # Build
-    croot && \
-    mka bacon
+# Sync the repositories
+/opt/crave/resync.sh && \
 
-    # Pull generated zip files
-    crave pull out/target/product/*/*.zip"
+# Set up build environment
+source build/envsetup.sh && \
+    
+# Lunch configuration
+lunch voltage_tissot-ap1a-userdebug && \
+    
+# Build
+croot \
+repo forall -c 'git lfs install && git lfs pull && git lfs checkout' \
+mka bacon"
 
-    # Pull generated img files
-    crave pull out/target/product/*/*.img
+# Pull generated zip files
+crave pull out/target/product/*/*.zip
 
-    # Upload zips to Telegram
-    # telegram-upload --to sdreleases out/target/product/*/*.zip
+# Pull generated img files
+crave pull out/target/product/*/*.img
 
-    # Upload to Github Releases
-    # curl -sf https://raw.githubusercontent.com/Meghthedev/Releases/main/headless.sh | sh
-"
-
+# Upload zips to Telegram
+# telegram-upload --to sdreleases out/target/product/*/*.zip
+    
+# Upload to Github Releases
+# curl -sf https://raw.githubusercontent.com/Meghthedev/Releases/main/headless.sh | sh
 # Clean up build artifacts (if needed)
 # rm -rf out/target/product/*
