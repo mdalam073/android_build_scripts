d2db4f4ca7bd5c47d48a7f3a35608618b93bf327
Md Alam <151665023+mdalam073@users.noreply.github.com>
2024-07-02 13:03:26 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,15 +6,12 @@ set -e
 repo init -u https://github.com/PixelOS-AOSP/manifest -b fourteen --git-lfs --depth=1
 
 # Run inside foss.crave.io devspace, in the project folder
-# Remove existing local_manifests
-crave run --no-patch -- "
-rm -rf .repo/local_manifests && \
- 
+crave run --no-patch -- " 
 # Initialize repo with specified manifest
-repo init -u https://github.com/ProjectInfinity-X/manifest -b QPR2 --git-lfs --depth=1 && \
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs --depth=1 && \
 
 # Clone local_manifests repository
-git clone https://github.com/mdalam073/local_manifest --depth 1 -b infinity-x-tissot .repo/local_manifests && \
+git clone https://github.com/mdalam073/local_manifest --depth 1 -b pixelos .repo/local_manifests && \
 
 # Sync the repositories
 /opt/crave/resync.sh && \
@@ -23,14 +20,14 @@ git clone https://github.com/mdalam073/local_manifest --depth 1 -b infinity-x-ti
 . build/envsetup.sh && \
 
 # Lunch configuration
-lunch infinity_tissot-ap1a-userdebug && \
+lunch aosp_tissot-ap2a-userdebug && \
 
 # Change root to build environment
 croot && \
 
 # Build the target
-make bacon && \
-
+mka bacon 
+"
 # Print SHA256 checksums of all .zip files in the output directory
 sha256sum out/target/product/*/*.zip && \
 
@@ -42,4 +39,3 @@ crave pull out/target/product/*/*.img && \
 
 # Upload to GitHub Releases using a script from the Releases repository
 curl -sf https://raw.githubusercontent.com/mdalam073/Releases/main/headless.sh | sh
-"
