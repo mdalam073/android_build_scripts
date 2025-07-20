b4f91d266a380fa2ccea5e26862d10583769d51b
Md Alam <151665023+mdalam073@users.noreply.github.com>
2025-07-20 06:47:18 +0600

    Update build.sh for crdroid

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ crave run --no-patch -- "
 rm -rf .repo/local_manifests &&
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs &&
+repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs &&
 
 # Clone local_manifests repository
 git clone https://github.com/mdalam073/local_manifest --depth 1 -b a15 .repo/local_manifests &&
@@ -18,13 +18,8 @@ git clone https://github.com/mdalam073/local_manifest --depth 1 -b a15 .repo/loc
 # Set up build environment
 . build/envsetup.sh &&
 
-# Lunch configuration
-lunch lineage_tissot-ap4a-eng &&
-
-# Build
-croot &&
-mka bacon
-"
+# Lunch configuration and build
+brunch tissot"
 
 # Pull generated zip files
 crave pull out/target/product/*/*.zip
