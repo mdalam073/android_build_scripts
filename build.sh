a392cade9f510a7ddf85898078aca2c0df5dc24c
Md Alam <151665023+mdalam073@users.noreply.github.com>
2025-08-14 15:26:44 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ crave run --no-patch -- "
 rm -rf .repo/local_manifests &&
 
 # Initialize repo with specified manifest
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault &&
+repo init -u https://github.com/alphadroid-project/manifest -b alpha-15.2 --git-lfs &&
 
 # Clone local_manifests repository
 git clone https://github.com/mdalam073/local_manifest --depth 1 -b infinity-15 .repo/local_manifests &&
@@ -16,17 +16,16 @@ rm -rf .repo/projects/prebuilts/clang/host/linux-x86.git &&
 rm -rf prebuilts/clang/host/linux-x86 &&
 
 # Sync the repositories
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) &&
+repo sync --force-sync &&
 
 # Set up build environment
 . build/envsetup.sh &&
 
 # Lunch configuration
-lunch infinity_tissot-userdebug &&
+lunch alpha_tissot-userdebug &&
 
 # Build
-croot &&
-mka bacon
+make bacon
 "
 
 # Pull generated zip files
