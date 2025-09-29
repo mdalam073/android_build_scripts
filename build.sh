052e6d3f523724715f5ded64aad7ffe9746ffbe2
Md Alam <151665023+mdalam073@users.noreply.github.com>
2025-09-29 12:48:52 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ crave run --no-patch -- "
 rm -rf .repo/local_manifests &&
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs &&
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs &&
 
 # Clone local_manifests repository
-git clone https://github.com/mdalam073/local_manifest --depth 1 -b a15 .repo/local_manifests &&
+git clone https://github.com/mdalam073/local_manifest --depth 1 -b a16 .repo/local_manifests &&
 
 rm -rf prebuilts/clang/host/linux-x86 &&
 
